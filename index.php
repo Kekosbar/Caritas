d48aac2737bdb6f819663960cfb3ecb7c8b8c4b5
caique <caiquecoelho2000@gmail.com>
2018-09-24 11:50:52 -0300

    Estou testando um commit

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+
 <html>
     <head>
         <meta charset="UTF-8">
@@ -11,6 +7,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
+        // Comentario para testar novo comit
         echo 'Testando o GITHUB';
         ?>
     </body>
